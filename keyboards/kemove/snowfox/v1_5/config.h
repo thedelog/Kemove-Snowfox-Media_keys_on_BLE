66f5a559f1d05d2c699bd1fbc362288d3f8a3dfb
thedelog <soyaslan@gmail.com>
2023-04-10 17:01:44 +0300

    autoshift and capsword added

--- keyboards/kemove/snowfox/v1_5/config.h
+++ keyboards/kemove/snowfox/v1_5/config.h
@@ -21,15 +21,16 @@
 #define MAX_LAYERS          5
 #define DIP_SWITCH_PINS     { LINE_DIP0 }
 
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 /*
-#define KEYMAP( 
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, 
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, 
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      
-    K30,      K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      
-    K40, K41, K42,                K43,           K44,  K45,   K46,   K47  
-) { 
-             COL0   COL1   COL2   COL3   COL4   COL5   COL6   COL7   COL8     
+#define KEYMAP(
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,
+    K30,      K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,
+    K40, K41, K42,                K43,           K44,  K45,   K46,   K47
+) {
+             COL0   COL1   COL2   COL3   COL4   COL5   COL6   COL7   COL8
      ROW0   { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,  },
      ROW1   { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,  },
      ROW2   { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,  },
